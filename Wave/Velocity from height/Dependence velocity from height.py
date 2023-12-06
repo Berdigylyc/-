a8eee4916bc2bb1b626ce2547f4810a3413d2014
Berdigylyc <146063996+Berdigylyc@users.noreply.github.com>
2023-12-06 11:16:26 +0300

    Dependence velocity from height.py

--- Wave/Velocity from height/Dependence velocity from height.py
+++ Wave/Velocity from height/Dependence velocity from height.py
@@ -3,18 +3,23 @@ import numpy as np
 
 # Given x and y values
 x_values = [20, 40, 60, 80, 100]
-y_values = [0.46, 0.72, 0.833, 0.92, 1.029]
+y_values = [460, 720, 833, 920, 1029]
 
 # Calculate the square root of each x value to get y values
-x_values = np.sqrt(x_values)
+y_values = np.sqrt(y_values)
 
 # Plot the data points
 plt.scatter(x_values, y_values,)
 plt.plot(x_values, y_values, )
 
-plt.xlabel('Square root of height (mm^1/2)')
-plt.ylabel('Velocity (m/s)')
-plt.title('Dependence velocity of wave and height')
+slope, intercept = np.polyfit(x_values, y_values, 1)
+
+# Print the slope
+print(f"Slope of the line: {1/slope}")
+
+plt.xlabel('Height (mm)')
+plt.ylabel('Square root of velocity (m/s^1/2)')
+plt.title('Dependence velocity of wave and height, g = 8.3 m/s^2')
 plt.legend()
 plt.grid(True)
 plt.show()
